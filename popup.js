bd3d75d401c59454d4281f605340909b9b4be30a
mattsroufe <matt.sroufe@powershop.co.nz>
2015-01-26 12:41:02 +1300

    adds ajax request to get api/links/new and functionality to add it to form

--- popup.js
+++ popup.js
@@ -1,7 +1,24 @@
 document.addEventListener('DOMContentLoaded', function () {
+  var $link_submission_url = document.querySelector('#link_submission_url'),
+      $link_submission_title = document.querySelector('#link_submission_title'),
+      $link_submission_content = document.querySelector('#link_submission_content');
+
   chrome.tabs.query({'active': true, 'windowId': chrome.windows.WINDOW_ID_CURRENT},
     function (tabs) {
-      document.querySelector('#link_submission_url').value = tabs[0].url;
+      var url = tabs[0].url,
+          xhr = new XMLHttpRequest();
+
+      $link_submission_url.value = url;
+      xhr.onreadystatechange = function () {
+        if ( xhr.readyState === 4 && xhr.status === 200 ) {
+          var link_submission = JSON.parse(xhr.responseText).link_submission;
+          $link_submission_title.value = link_submission.title;
+          $link_submission_content.value = link_submission.content;
+        }
+      };
+      xhr.open("GET", "http://localhost:3000/api/links/new?auth_token=JJ-MJE6og2ZSzAaz84gR&url=" + url, true);
+      xhr.setRequestHeader("Accept", "application/json");
+      xhr.send();
     }
   );
 });
