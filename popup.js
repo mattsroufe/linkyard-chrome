07ee3226d91f54033830534625c3dd9e3afaa6d2
Van Veñegas <van.venegas@powershop.co.nz>
2015-02-23 17:45:17 +1300

    Added auth and interaction support

--- popup.js
+++ popup.js
@@ -1,58 +1,167 @@
-BASE_URL = 'http://localhost:3000/api';
-var authToken = 'JJ-MJE6og2ZSzAaz84gR';
+BASE_URL = 'http://localhost:3000';
+API_URL = BASE_URL + '/api';
 
-$(function () {
-  var $linkSubmissionUrl = $('#link-submission-url'),
-      $linkSubmissionTitle = $('#link-submission-title'),
-      $linkSubmissionContent = $('#link-submission-content'),
-      $submitButton = $('input[type=submit]');
+// TODO: remember and use token in case we switch away from cookie-based
+// auth, or have a separate sessions for the webapp and the extension.
+var authToken = 'dummy';
 
+$(function() {
+  var $linkSubmissionUrl = $('#link-submission-url');
+  var $linkSubmissionTitle = $('#link-submission-title');
+  var $linkSubmissionContent = $('#link-submission-content');
+  var $submitForm = $('form#submit');
+  var $submitButton = $('form#submit').find('input[type=submit]');
+  var $loginOrSignup = $('#login-or-signup');
+  var $loginForm = $('form#login-form');
+  var $connectionErrorMessage = $('#connection-error');
+  var $spinner = $('#spinner');
+
+  $spinner.show();
+
+  // Pre-submission query
   chrome.tabs.query({'active': true, 'windowId': chrome.windows.WINDOW_ID_CURRENT},
-    function (tabs) {
+    function(tabs) {
       var currentUrl = tabs[0].url;
+
+      $submitForm.hide();
 
       $linkSubmissionUrl.val(currentUrl);
       $.ajax({
         type: 'GET',
-        url: BASE_URL + '/links/new',
+        url: API_URL + '/links/new',
         headers: {
           'Accept': 'application/json'
         },
         data: {
-          auth_token: authToken,
-          url: currentUrl
+          // auth_token: authToken,
+          url: currentUrl,
         }
       })
-      .done(function (data) {
+      .error(function(data, status, error) {
+        if (error == 'Unauthorized') {
+          $spinner.hide();
+          $loginOrSignup.show();
+        } else {
+          $spinner.hide();
+          $connectionErrorMessage.show();
+        }
+      })
+      .done(function(data, status, xhr) {
+        // console.log(data);
+        // console.log(status);
+        // console.log(xhr);
+
+        $spinner.hide();
+        $submitForm.show();
+
         var linkSubmission = data.link_submission;
         $linkSubmissionTitle.val(linkSubmission.title).removeAttr('disabled');
         $linkSubmissionContent.val(linkSubmission.content).removeAttr('disabled');
         $submitButton.val('Add').removeAttr('disabled');
+
+        $.each(linkSubmission.link_interactions, function(index, interaction) {
+          var id = interaction.id;
+          html = '<label for="interaction-' + id
+            + '"> <input type="checkbox" id="interaction-' + id
+            + '" name="link_interactions_' + id
+            + '" />' + interaction.name + '</label>';
+          $('#link-submission-interactions').append(html);
+        });
       });
     }
   );
 
-  $submitButton.click(function (event) {
+  // Link submission
+  $submitForm.submit(function(event) {
     event.preventDefault();
+
+    var link_submission = {
+      link_interactions: []
+    };
+
+    $.each($('#submit').find(':input').serializeArray(),
+           function(index, input) {
+             name = input.name;
+             value = input.value;
+
+             if (name.indexOf('link_interactions_') == 0) {
+               if (value == 'on') {
+                 id = name.replace('link_interactions_', '');
+                 link_submission.link_interactions.push({ id: id, checked: '1' });
+               }
+             } else {
+               link_submission[name] = value;
+             }
+           });
+
+    data = JSON.stringify({
+      //'auth_token': authToken,
+      'link_submission': link_submission,
+    });
+
+    // console.log(data);
+
     $.ajax({
       type: 'POST',
-      url: BASE_URL + '/links',
+      url: API_URL + '/links',
+      contentType: 'application/json',
+      headers: {
+        'Accept': 'application/json',
+      },
+      data: data
+    })
+    .error(function(data, status, error) {
+      if (error == 'Unauthorized') {
+        $submitForm.hide();
+        $loginOrSignup.show();
+      } else {
+        $submitForm.hide();
+        $connectionErrorMessage.show();
+      }
+    })
+    .done(function(data) {
+      $('body').html('<p class="success">Link successfully added.</p>');
+    });
+  });
+
+  // Auth form
+  $loginForm.submit(function(event) {
+    event.preventDefault();
+
+    $.ajax({
+      type: 'POST',
+      url: API_URL + '/sessions',
       headers: {
         'Accept': 'application/json'
       },
       data: {
-        auth_token: authToken,
-        link_submission: {
-          url: $linkSubmissionUrl.val(),
-          tags: $('#link-submission-tags').val(),
-          description: $('#link-submission-description').val(),
-          title: $linkSubmissionTitle.val(),
-          content: $linkSubmissionContent.val()
+        'user': {
+          'email': $('#email').val(),
+          'password': $('#password').val(),
+          'remember_me': '1'
         }
       }
     })
-    .done(function (data) {
-      $('body').html('<p class="success">Link successfully added.</p>');
+    .error(function(data, status, error) {
+      if (error == 'Unauthorized') {
+        $loginOrSignup.show();
+      } else {
+        $connectionErrorMessage.show();
+      }
+    })
+    .done(function(data, status, xhr) {
+      location.reload()
     });
+
+    $(this).find('input').attr('disabled', 'disabled');
+  });
+
+  // Miscellaneous hooks
+  $('#try-again').click(function() {
+    location.reload();
+  });
+
+  $('#signup').click(function() {
+    window.open(BASE_URL);
   });
 });
